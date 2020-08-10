d29d55999bec0b549704d9b8fbffb9d8a9dfb0b7
refszult <refszult@gmail.com>
2020-08-10 16:47:05 +0300

    small refactoring

--- Repository.php
+++ Repository.php
@@ -5,45 +5,12 @@ class Repository
 {
     private PDO $pdo;
 
-    public function __construct()
+    public function __construct(array $dbSettings)
     {
-        $this->pdo = new PDO("mysql:host=localhost;dbname=test", 'root', '');
+        $this->pdo = new PDO("mysql:host={$dbSettings['HOST']};dbname={$dbSettings['DB_NAME']}", $dbSettings['USERNAME'], $dbSettings['PASSWORD']);
     }
 
-    /**
-     * @param array $condition
-     * @return bool
-     */
-
-    public function exist(array $condition)
-    {
-        $stmt = $this->pdo->prepare("SELECT id FROM visits {$this->where($condition)}");
-        $stmt->execute();
-        $res = $stmt->fetch();
-        if ($res) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * @param array $condition
-     * @return bool
-     */
-    public function updateCount(array $condition)
-    {
-        $stmt = $this->pdo->prepare("UPDATE visits SET views_count = views_count + 1 {$this->where($condition)}");
-        $stmt->execute();
-
-        return $stmt->fetch();
-    }
-
-    /**
-     * @param array $data
-     * @return bool
-     */
-    public function create(array $data)
+    public function createOrUpdate(array $data)
     {
         $keys = [];
         $values = [];
@@ -53,30 +20,10 @@ class Repository
             array_push($values, "'" . $value . "'");
         }
 
-        $stmt = $this->pdo->prepare("INSERT INTO visits (" . implode(',', $keys) . ") VALUES (" . implode(',', $values) . ")");
+        $stmt = $this->pdo->prepare("INSERT INTO visits (" . implode(',', $keys) . ")
+         VALUES (" . implode(',', $values) . ") ON DUPLICATE KEY UPDATE views_count=views_count+1");
         $stmt->execute();
 
         return $stmt->fetch();
-    }
-
-    /**
-     * @param array $dataWhere
-     * @return string
-     */
-    private function where(array $dataWhere)
-    {
-        $i = 1;
-        $where = '';
-
-        foreach ($dataWhere as $key => $value) {
-            if ($i == 1) {
-                $where = "WHERE $key='$value' ";
-                $i++;
-            } else {
-                $where .= "AND $key='$value' ";
-            }
-        }
-
-        return $where;
     }
 }
--- banner.php
+++ banner.php
@@ -1,9 +1,15 @@
 <?php
 
 require 'VisitService.php';
+require 'Repository.php';
 
-$visitService = new VisitService($_SERVER);
-$visitService->savingVisit();
+$visitService = new VisitService(new Repository(parse_ini_file('.env', false, INI_SCANNER_RAW)));
+$visitService->savingVisit([
+    'ip' => $_SERVER['REMOTE_ADDR'],
+    'user_agent' => $_SERVER['HTTP_USER_AGENT'],
+    'view_date' => date("y.m.d"),
+    'page_url' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']
+]);
 
 $img = file_get_contents('https://i.picsum.photos/id/496/536/354.jpg?hmac=U8UJd7a1T_tp4baF1lfEma_vCZI9XA6ou60WNjRWC4s');
 echo $img;
